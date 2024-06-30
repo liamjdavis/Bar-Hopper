5c8ff00b8300f488d0d2bfb21df404e81b858a9c
Liam Davis <davisliam123@gmail.com>
2024-06-30 16:37:30 -0700

    added layout pages for main pages

---
+++ frontend/components/layout/NavBar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
+import { IonIcons } from '@expo/vector-icons';
+
+import map from '../../assets/map-logo.png';
+import friends from '../../assets/friends-logo.png';
+import promos from '../../assets/promos-logo.png';
+
+const Tab = createBottomTabNavigator();
+
+return();
